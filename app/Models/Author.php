9576db80c9e53af92ca5ef7632b95d2a4fa22261
PsychedelicMonkey <ryan.getz.96@gmail.com>
2024-11-21 19:39:40 -0700

    Blog models

---
+++ app/Models/Author.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Author extends Model
+{
+    /** @use HasFactory<\Database\Factories\AuthorFactory> */
+    use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $table = 'blog_authors';
+
+    /**
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'bio',
+    ];
+
+    /** @return HasMany<Post> */
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class, 'blog_author_id');
+    }
+}
