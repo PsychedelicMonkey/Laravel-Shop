5f30b1a6806393e206f941da46fe903a898088e1
PsychedelicMonkey <ryan.getz.96@gmail.com>
2024-11-22 20:22:01 -0700

    Customer resource

--- app/Filament/Resources/Shop/CustomerResource.php
+++ app/Filament/Resources/Shop/CustomerResource.php
@@ -28,53 +28,85 @@ class CustomerResource extends Resource
 
     public static function form(Form $form): Form
     {
-        // TODO: form
-
         return $form
             ->schema([
-                Forms\Components\TextInput::make('user_id')
-                    ->numeric(),
-                Forms\Components\TextInput::make('name')
-                    ->required(),
-                Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->required(),
-                Forms\Components\TextInput::make('gender')
-                    ->required(),
-                Forms\Components\TextInput::make('phone')
-                    ->tel(),
-                Forms\Components\DatePicker::make('birthday'),
-            ]);
+                Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->maxLength(255)
+                            ->required(),
+
+                        Forms\Components\TextInput::make('email')
+                            ->email()
+                            ->maxLength(255)
+                            ->required()
+                            ->unique(Customer::class, 'email', ignoreRecord: true),
+
+                        Forms\Components\TextInput::make('phone')
+                            ->maxLength(255)
+                            ->tel(),
+
+                        Forms\Components\DatePicker::make('birthday')
+                            ->maxDate(now())
+                            ->native(false)
+                            ->placeholder(fn () => now()->format('M d, Y')),
+
+                        Forms\Components\ToggleButtons::make('gender')
+                            ->grouped()
+                            ->inline()
+                            ->options([
+                                'male' => 'Male',
+                                'female' => 'Female',
+                                'other' => 'Other',
+                            ])
+                            ->required(),
+                    ])
+                    ->columns(2)
+                    ->columnSpan(['lg' => fn (?Customer $record) => $record === null ? 3 : 2]),
+
+                Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\Placeholder::make('created_at')
+                            ->label('Created at')
+                            ->content(fn (Customer $record): ?string => $record->created_at?->diffForHumans()),
+
+                        Forms\Components\Placeholder::make('updated_at')
+                            ->label('Last modified at')
+                            ->content(fn (Customer $record): ?string => $record->updated_at?->diffForHumans()),
+                    ])
+                    ->columnSpan(['lg' => 1])
+                    ->hidden(fn (?Customer $record) => $record === null),
+            ])
+            ->columns(3);
     }
 
     public static function table(Table $table): Table
     {
-        // TODO: table
-
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('user_id')
-                    ->numeric()
-                    ->sortable(),
                 Tables\Columns\TextColumn::make('name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('email')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('gender')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('phone')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('birthday')
-                    ->date()
+                    ->searchable(isIndividual: true)
                     ->sortable(),
+
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Email address')
+                    ->searchable(isIndividual: true, isGlobal: false)
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable()
+                    ->sortable(),
+
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
+
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
+
                 Tables\Columns\TextColumn::make('deleted_at')
                     ->dateTime()
                     ->sortable()
@@ -117,5 +149,10 @@ class CustomerResource extends Resource
             ->withoutGlobalScopes([
                 SoftDeletingScope::class,
             ]);
+    }
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['name', 'email'];
     }
 }
