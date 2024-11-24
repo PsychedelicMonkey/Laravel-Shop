908240d07f8504147228dbfad209598cc04ef387
PsychedelicMonkey <ryan.getz.96@gmail.com>
2024-11-24 07:23:46 -0700

    Install settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,19 @@ class AppServiceProvider extends ServiceProvider
     {
         // Eloquent settings.
         Model::shouldBeStrict(! $this->app->isProduction());
+
+        // Force HTTPS when in production.
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // Default password rules.
+        Password::defaults(function () {
+            $rules = Password::min(8);
+
+            return $this->app->isProduction()
+                ? $rules->letters()->mixedCase()->numbers()->symbols()
+                : $rules;
+        });
     }
 }
