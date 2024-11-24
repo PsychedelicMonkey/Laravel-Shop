71c3841027b0d440d3546abff91fcba5143c9c3e
PsychedelicMonkey <ryan.getz.96@gmail.com>
2024-11-24 08:11:38 -0700

    Install sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,15 @@ use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements FilamentUser
 {
+    use HasApiTokens;
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
     use Notifiable;
 
     /**
