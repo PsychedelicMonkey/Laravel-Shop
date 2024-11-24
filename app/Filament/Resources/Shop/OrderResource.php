853895f3d421fe69ecc6c0b63d004bf2fe5bb9db
PsychedelicMonkey <ryan.getz.96@gmail.com>
2024-11-23 17:48:55 -0700

    Order form

--- app/Filament/Resources/Shop/OrderResource.php
+++ app/Filament/Resources/Shop/OrderResource.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\Resources\Shop;
 
+use App\Enums\OrderStatus;
+use App\Filament\Clusters\Products\Resources\ProductResource;
 use App\Filament\Resources\Shop\OrderResource\Pages;
 use App\Filament\Resources\Shop\OrderResource\Widgets;
 use App\Models\Shop\Order;
+use App\Models\Shop\Product;
+use App\Models\User;
+use Carbon\Carbon;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -13,6 +18,8 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Random\RandomException;
+use Squire\Models\Currency;
 
 class OrderResource extends Resource
 {
@@ -28,28 +35,48 @@ class OrderResource extends Resource
 
     protected static ?int $navigationSort = 1;
 
+    /**
+     * @throws RandomException
+     */
     public static function form(Form $form): Form
     {
-        // TODO: form
-
         return $form
             ->schema([
-                Forms\Components\TextInput::make('shop_customer_id')
-                    ->numeric(),
-                Forms\Components\TextInput::make('number')
-                    ->required(),
-                Forms\Components\TextInput::make('total_price')
-                    ->numeric(),
-                Forms\Components\TextInput::make('status')
-                    ->required(),
-                Forms\Components\TextInput::make('currency')
-                    ->required(),
-                Forms\Components\TextInput::make('shipping_price')
-                    ->numeric(),
-                Forms\Components\TextInput::make('shipping_method'),
-                Forms\Components\Textarea::make('notes')
-                    ->columnSpanFull(),
-            ]);
+                Forms\Components\Group::make()
+                    ->schema([
+                        Forms\Components\Section::make()
+                            ->schema(static::getDetailsFormSchema())
+                            ->columns(2),
+
+                        Forms\Components\Section::make('Order Items')
+                            ->headerActions([
+                                Forms\Components\Actions\Action::make('reset')
+                                    ->modalHeading('Are you sure?')
+                                    ->modalDescription('All existing items will be removed from the order.')
+                                    ->requiresConfirmation()
+                                    ->color('danger')
+                                    ->action(fn (Forms\Set $set) => $set('items', [])),
+                            ])
+                            ->schema([
+                                static::getItemsRepeater(),
+                            ]),
+                    ])
+                    ->columnSpan(['lg' => fn (?Order $record) => $record === null ? 3 : 2]),
+
+                Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\Placeholder::make('created_at')
+                            ->label('Created at')
+                            ->content(fn (Order $record): ?string => $record->created_at?->diffForHumans()),
+
+                        Forms\Components\Placeholder::make('updated_at')
+                            ->label('Last modified at')
+                            ->content(fn (Order $record): ?string => $record->updated_at?->diffForHumans()),
+                    ])
+                    ->columnSpan(['lg' => 1])
+                    ->hidden(fn (?Order $record) => $record === null),
+            ])
+            ->columns(3);
     }
 
     public static function table(Table $table): Table
@@ -78,6 +105,7 @@ class OrderResource extends Resource
                     ]),
 
                 Tables\Columns\TextColumn::make('currency')
+                    ->getStateUsing(fn ($record): ?string => Currency::find($record->currency)?->name ?? null)
                     ->searchable()
                     ->sortable()
                     ->toggleable(),
@@ -110,7 +138,38 @@ class OrderResource extends Resource
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
 
-                // TODO: created_at filter.
+                Tables\Filters\Filter::make('created_at')
+                    ->form([
+                        Forms\Components\DatePicker::make('created_from')
+                            ->native(false)
+                            ->placeholder(fn ($state): string => 'Dec 18, ' . now()->format('Y')),
+
+                        Forms\Components\DatePicker::make('created_until')
+                            ->native(false)
+                            ->placeholder(fn ($state): string => now()->format('M d, Y')),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['created_from'] ?? null,
+                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                            )
+                            ->when(
+                                $data['created_until'] ?? null,
+                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                            );
+                    })
+                    ->indicateUsing(function (array $data): array {
+                        $indicators = [];
+                        if ($data['created_from'] ?? null) {
+                            $indicators['created_from'] = 'Order from ' . Carbon::parse($data['created_from'])->toFormattedDateString();
+                        }
+                        if ($data['created_until'] ?? null) {
+                            $indicators['created_until'] = 'Order until ' . Carbon::parse($data['created_until'])->toFormattedDateString();
+                        }
+
+                        return $indicators;
+                    }),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -184,5 +243,137 @@ class OrderResource extends Resource
         $modelClass = static::$model;
 
         return (string) $modelClass::where('status', 'new')->count();
+    }
+
+    /** @return Forms\Components\Component[]
+     *
+     * @throws RandomException
+     */
+    public static function getDetailsFormSchema(): array
+    {
+        return [
+            Forms\Components\TextInput::make('number')
+                ->default('OR-' . random_int(100000, 999999))
+                ->disabled()
+                ->dehydrated()
+                ->maxLength(32)
+                ->required()
+                ->unique(Order::class, 'number', ignoreRecord: true),
+
+            Forms\Components\Select::make('shop_customer_id')
+                ->relationship('customer', 'name')
+                ->required()
+                ->searchable()
+                ->createOptionForm([
+                    Forms\Components\TextInput::make('name')
+                        ->maxLength(255)
+                        ->required(),
+
+                    Forms\Components\TextInput::make('email')
+                        ->label('Email address')
+                        ->email()
+                        ->maxLength(255)
+                        ->required()
+                        ->unique(User::class, 'email', ignoreRecord: true),
+
+                    Forms\Components\TextInput::make('phone')
+                        ->maxLength(255),
+
+                    Forms\Components\Select::make('gender')
+                        ->options([
+                            'male' => 'Male',
+                            'female' => 'Female',
+                            'other' => 'Other',
+                        ])
+                        ->placeholder('Select gender')
+                        ->required()
+                        ->native(false),
+                ])
+                ->createOptionAction(function (Forms\Components\Actions\Action $action) {
+                    return $action
+                        ->modalHeading('Create customer')
+                        ->modalSubmitActionLabel('Create customer')
+                        ->modalWidth('lg');
+                }),
+
+            Forms\Components\ToggleButtons::make('status')
+                ->inline()
+                ->options(OrderStatus::class)
+                ->required(),
+
+            Forms\Components\Select::make('currency')
+                ->getSearchResultsUsing(fn (string $query) => Currency::where('name', 'like', "%{$query}%")->pluck('name', 'id'))
+                ->getOptionLabelUsing(fn ($value): ?string => Currency::firstWhere('id', $value)?->getAttribute('name'))
+                ->required()
+                ->searchable(),
+
+            // TODO: address form
+
+            Forms\Components\RichEditor::make('notes')
+                ->columnSpanFull(),
+        ];
+    }
+
+    public static function getItemsRepeater(): Forms\Components\Repeater
+    {
+        return Forms\Components\Repeater::make('items')
+            ->relationship()
+            ->schema([
+                Forms\Components\Select::make('shop_product_id')
+                    ->label('Product')
+                    ->options(Product::query()->pluck('name', 'id'))
+                    ->required()
+                    ->reactive()
+                    ->afterStateUpdated(fn (Forms\Set $set, ?string $state) => $set('unit_price', Product::find($state)?->price ?? 0))
+                    ->distinct()
+                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                    ->columnSpan([
+                        'md' => 5,
+                    ])
+                    ->searchable(),
+
+                Forms\Components\TextInput::make('qty')
+                    ->label('Quantity')
+                    ->numeric()
+                    ->default(1)
+                    ->columnSpan([
+                        'md' => 2,
+                    ])
+                    ->required(),
+
+                Forms\Components\TextInput::make('unit_price')
+                    ->label('Unit Price')
+                    ->disabled()
+                    ->dehydrated()
+                    ->numeric()
+                    ->required()
+                    ->columnSpan([
+                        'md' => 3,
+                    ]),
+            ])
+            ->extraItemActions([
+                Forms\Components\Actions\Action::make('openProduct')
+                    ->tooltip('Open product')
+                    ->icon('heroicon-m-arrow-top-right-on-square')
+                    ->url(function (array $arguments, Forms\Components\Repeater $component): ?string {
+                        $itemData = $component->getRawItemState($arguments['item']);
+
+                        $product = Product::find($itemData['shop_product_id']);
+
+                        if (! $product) {
+                            return null;
+                        }
+
+                        return ProductResource::getUrl('edit', ['record' => $product]);
+                    }, shouldOpenInNewTab: true)
+                    ->hidden(fn (array $arguments, Forms\Components\Repeater $component): bool => blank($component->getRawItemState($arguments['item'])['shop_product_id'])),
+            ])
+            ->orderColumn('sort')
+            ->defaultItems(1)
+            ->hiddenLabel()
+            ->columns([
+                'md' => 10,
+            ])
+            ->required();
     }
 }
--- app/Filament/Resources/Shop/OrderResource/Pages/CreateOrder.php
+++ app/Filament/Resources/Shop/OrderResource/Pages/CreateOrder.php
@@ -3,9 +3,54 @@
 namespace App\Filament\Resources\Shop\OrderResource\Pages;
 
 use App\Filament\Resources\Shop\OrderResource;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Wizard\Step;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\CreateRecord;
+use Random\RandomException;
 
 class CreateOrder extends CreateRecord
 {
+    use CreateRecord\Concerns\HasWizard;
+
     protected static string $resource = OrderResource::class;
+
+    /**
+     * @throws RandomException
+     */
+    public function form(Form $form): Form
+    {
+        return parent::form($form)
+            ->schema([
+                Wizard::make($this->getSteps())
+                    ->startOnStep($this->getStartStep())
+                    ->cancelAction($this->getCancelFormAction())
+                    ->submitAction($this->getSubmitFormAction())
+                    ->skippable($this->hasSkippableSteps())
+                    ->contained(false),
+            ])
+            ->columns(null);
+    }
+
+    /** @return Step[]
+     *
+     * @throws RandomException
+     */
+    public function getSteps(): array
+    {
+        return [
+            Step::make('Order Details')
+                ->schema([
+                    Section::make()->schema(OrderResource::getDetailsFormSchema())->columns(),
+                ]),
+
+            Step::make('Order Items')
+                ->schema([
+                    Section::make()->schema([
+                        OrderResource::getItemsRepeater(),
+                    ]),
+                ]),
+        ];
+    }
 }
