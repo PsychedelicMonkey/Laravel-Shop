5932e6540337182f53b620f3fad92f9d9601664e
PsychedelicMonkey <ryan.getz.96@gmail.com>
2024-11-23 19:41:14 -0700

    Relation managers

---
+++ app/Filament/Resources/Shop/CustomerResource/RelationManagers/AddressesRelationManager.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources\Shop\CustomerResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Squire\Models\Country;
+
+class AddressesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'addresses';
+
+    protected static ?string $recordTitleAttribute = 'full_address';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('street')
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('zip')
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('city')
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('state')
+                    ->maxLength(255),
+
+                Forms\Components\Select::make('country')
+                    ->getOptionLabelUsing(fn ($value): ?string => Country::firstWhere('id', $value)?->getAttribute('name'))
+                    ->getSearchResultsUsing(fn (string $query) => Country::where('name', 'like', "%{$query}%")->pluck('name', 'id'))
+                    ->searchable(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('street'),
+
+                Tables\Columns\TextColumn::make('zip'),
+
+                Tables\Columns\TextColumn::make('city'),
+
+                Tables\Columns\TextColumn::make('country')
+                    ->formatStateUsing(fn ($state): ?string => Country::find($state)?->name ?? null),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\AttachAction::make(),
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DetachAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DetachBulkAction::make(),
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
--- app/Filament/Resources/Shop/OrderResource.php
+++ app/Filament/Resources/Shop/OrderResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Shop;
 use App\Enums\OrderStatus;
 use App\Filament\Clusters\Products\Resources\ProductResource;
 use App\Filament\Resources\Shop\OrderResource\Pages;
+use App\Filament\Resources\Shop\OrderResource\RelationManagers;
 use App\Filament\Resources\Shop\OrderResource\Widgets;
 use App\Forms\Components\AddressForm;
 use App\Models\Shop\Order;
@@ -193,7 +194,7 @@ class OrderResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\PaymentsRelationManager::class,
         ];
     }
 
