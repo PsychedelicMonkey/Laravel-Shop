f3ce6ffffc5856d6e305705da794276f5581e3bc
PsychedelicMonkey <ryan.getz.96@gmail.com>
2024-11-24 07:08:43 -0700

    Import and export actions

--- database/migrations/2024_11_24_132732_create_notifications_table.php
+++ database/migrations/2024_11_24_132732_create_notifications_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,7 +16,13 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('type');
             $table->morphs('notifiable');
-            $table->text('data');
+
+            if (DB::getDriverName() === 'pgsql') {
+                $table->json('data');
+            } else {
+                $table->text('data');
+            }
+
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
