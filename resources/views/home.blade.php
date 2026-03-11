313f9c483097749eeebdbca0d087a7647e75ead0
PsychedelicMonkey <ryan.getz.96@gmail.com>
2026-03-11 07:08:35 -0600

    Layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="max-w-7xl mx-auto p-4 lg:p-6">
+        <article class="prose lg:prose-xl">
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, molestias!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi autem eligendi nam nisi recusandae tempore veritatis! Adipisci amet culpa harum id ipsam, labore minus nihil ut? Doloremque laudantium maiores reprehenderit!</p>
+            <ol>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet, consectetur.</li>
+                <li>Lorem ipsum.</li>
+            </ol>
+
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, ipsa?</h3>
+        </article>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name') }}</title>
+
+    @if(file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+</head>
+<body class="font-sans antialiased">
+    @include('layouts.navigation')
+
+    <div class="min-h-screen">
+        <main>
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
